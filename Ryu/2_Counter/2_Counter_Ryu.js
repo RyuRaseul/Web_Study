45aef71db3bb3c175b8f51572312c1c9875f6e86
RyuRaseul <113527604+RyuRaseul@users.noreply.github.com>
2023-06-06 14:54:04 +0000

    2_Counter

--- Ryu/2_Counter/2_Counter_Ryu.js
+++ Ryu/2_Counter/2_Counter_Ryu.js
@@ -1,23 +1,30 @@
+let Num = 0;
+
 const Count_Num = document.querySelector(".Count_box");
 
-/*const Btn_List = document.querySelectorAll(".btn_box");*/
+const Btn_List = document.querySelectorAll(".btn_box");
 
-const inc_btn = document.querySelector(".inc");
-const reset_btn = document.querySelector(".reset");
-const dec_btn = document.querySelector(".dec");
-
-inc_btn.addEventListener("click", inc_num);
-reset_btn.addEventListener("click", reset_num);
-dec_btn.addEventListener("click", dec_num);
-
-function inc_num(){
-    Count_Num.textContent = Count_Num.textContent * 1 + 1;
-}
-
-function reset_num(){
-    Count_Num.textContent = 0;
-}
-
-function dec_num(){
-    Count_Num.textContent = Count_Num.textContent * 1 - 1;
-}
+Btn_List.forEach(function(item){
+    item.addEventListener("click", function(e){
+        const Btn_Class = e.currentTarget.classList;
+        if(Btn_Class.contains("dec")){
+            Num--;
+        }
+        else if(Btn_Class.contains("inc")){
+            Num++;
+        }
+        else if(Btn_Class.contains("reset")){
+            Num = 0;
+        }
+        Count_Num.textContent = Num;
+        if(Num > 0){
+            Count_Num.style.color = "green";
+        }
+        else if(Num < 0){
+            Count_Num.style.color = "red";
+        }
+        else{
+            Count_Num.style.color = "black";
+        }
+    })
+});
