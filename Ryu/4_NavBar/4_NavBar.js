34657ef37593556ed22a3e692d1c34186d2a7f74
RyuRaseul <113527604+RyuRaseul@users.noreply.github.com>
2023-06-26 13:35:56 +0000

    4_NavBar

--- Ryu/4_NavBar/4_NavBar.js
+++ Ryu/4_NavBar/4_NavBar.js
@@ -3,14 +3,17 @@ const menu_btn = document.querySelector(".toggle_btn");
 const menu_contents = document.querySelectorAll(".menu_content");
 const icon_links = document.querySelectorAll(".icon_link")
 const hambuger_bar = document.querySelector(".nav_menu");
+const shadow_bar = document.querySelector(".title_container")
 menu_btn.addEventListener("click", menu_on_off);
 
 
 function menu_on_off(){
     menu_contents.forEach(element => {
-        element.classList.toggle('active');
+        element.classList.toggle('menu_active');
     });
     icon_links.forEach(element => {
-        element.classList.toggle('active');
+        element.classList.toggle('icons_active');
     });
+    document.body.classList.toggle('shadow_off');
+    shadow_bar.classList.toggle('shadow_on');
 }
