f4eb059e70a4fd9ddd59c02b5f0ecb4475b99b78
RyuRaseul <113527604+RyuRaseul@users.noreply.github.com>
2023-07-02 10:44:43 +0000

    6_Modal

--- Ryu/6_Modal/6_Modal.js
+++ Ryu/6_Modal/6_Modal.js
@@ -5,7 +5,12 @@ const modal_close_btn = document.querySelector('.xmark');
 
 modal_open_btn.addEventListener("click", modal_on);
 modal_close_btn.addEventListener("click", modal_off);
-modal_background.addEventListener("click", modal_off);
+
+modal_background.addEventListener("click", function(e){
+    if(e.target.classList.contains('modal-on')){
+        modal_off();
+    }
+});
 
 function modal_on(){
     modal_background.classList.add("modal-on");
