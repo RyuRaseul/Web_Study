b0edc165d68e2929ecebacdcc538d8b0c35e18a4
RyuRaseul <ydh88283072@gmail.com>
2023-07-03 11:37:36 +0000

    7_FAQ

--- Ryu/7_FAQ/7_FAQ.js
+++ Ryu/7_FAQ/7_FAQ.js
@@ -1,15 +1,13 @@
-const qna_body = document.querySelectorAll(".qna-container");
 const open_btn = document.querySelectorAll(".on-btn");
 const close_btn = document.querySelectorAll(".off-btn");
-const answer = document.querySelectorAll(".answer-body");
 let clicked_section = null;
+
 open_btn.forEach(function(item){
     item.addEventListener("click", function(e){
         if(clicked_section != null && clicked_section != e.target){
             clicked_section.parentElement.parentElement.parentElement.querySelector(".answer-body").classList.remove('on');
             clicked_section.classList.remove("hidden-btn");
             clicked_section.nextElementSibling.classList.add("hidden-btn");
-            console.log("?");
         }
         item.parentElement.parentElement.parentElement.querySelector(".answer-body").classList.add('on');
         e.target.classList.add("hidden-btn");
@@ -26,4 +24,20 @@ close_btn.forEach(function(item){
         clicked_section = null;
     });
 });
+
+/*
+const questions = document.querySelectorAll(".qna-container");
+
+questions.forEach(function(question){
+    const btn = question.querySelector(".btn-container");
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            if(item != question){
+                item.classList.remove("on");
+            }
+        })
+        question.classList.toggle("on");
+    })
+})
+*/
 
