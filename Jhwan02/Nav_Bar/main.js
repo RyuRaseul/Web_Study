bd2c0b86110f3d93f8becc715f3b1d5504fb754a
Jhwan02 <113514541+Jhwan02@users.noreply.github.com>
2023-07-03 05:12:51 +0000

    delete directory

--- Jhwan02/Nav_Bar/main.js
+++
@@ -1,6 +0,0 @@
-const navList = document.querySelector(".nav-bar__list");
-const navHambuger = document.querySelector(".nav-bar__hambuger");
-
-navHambuger.addEventListener("click", () => {
-    navList.classList.toggle("active");
-})
