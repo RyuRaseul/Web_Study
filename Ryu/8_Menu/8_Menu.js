0c1b1aad1406f80873613d460e8208b0c705d2bb
RyuRaseul <ydh88283072@gmail.com>
2023-07-06 14:28:15 +0000

    8_Menu

---
+++ Ryu/8_Menu/8_Menu.js
@@ -0,0 +1,66 @@
+const menu_set =[
+    {
+        category: "Breakfast",
+        img: "img/American-Breakfast.jpg",
+        name: "American Breakfast",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Breakfast",
+        img: "img/Pancakes.jpg",
+        name: "Pancakes",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Lunch",
+        img: "img/Burgers.jpg",
+        name: "Burgers",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Lunch",
+        img: "img/Hotdogs.jpg",
+        name: "Hotdogs",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Dinner",
+        img: "img/Baked-Stuffed-Lobster.jpg",
+        name: "Baked Stuffed Lobster",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Dinner",
+        img: "img/Pizza.jpg",
+        name: "Pizza",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Shakes",
+        img: "img/Milk_Shake.jpg",
+        name: "Milk Shake",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    },
+
+    {
+        category: "Shake",
+        img: "img/Berries_Smoothie.jpg",
+        name: "Berries Smoothie",
+        price: "0$",
+        content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae officiis numquam animi beatae, excepturi nemo at repudiandae libero obcaecati ipsam doloribus, nam quos reiciendis, odio velit magni sunt expedita necessitatibus!"
+    }
+];
+
