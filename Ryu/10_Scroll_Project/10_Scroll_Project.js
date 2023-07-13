4e7482cfcc304cdffe6b8fff9f4d54d34190b548
RyuRaseul <ydh88283072@gmail.com>
2023-07-13 13:42:01 +0000

    10_Scroll_Project

--- Ryu/10_Scroll_Project/10_Scroll_Project.js
+++ Ryu/10_Scroll_Project/10_Scroll_Project.js
@@ -2,3 +2,12 @@ var today = new Date();
 const date_content = document.querySelector(".year-container");
 
 date_content.innerText = today.getFullYear() + ".";
+
+const slide_bar = document.querySelector(".bar-container");
+const links = document.querySelector(".link-container");
+
+slide_bar.addEventListener("click", show_links);
+
+function show_links(){
+    links.classList.toggle('slide');
+};
