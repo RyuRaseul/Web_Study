7cab5de3f770be42ad9cdce6a6efe4466bbd0cb0
RyuRaseul <ydh88283072@gmail.com>
2023-07-16 14:47:31 +0000

    10_Scroll_Project

--- Ryu/10_Scroll_Project/10_Scroll_Project.js
+++ Ryu/10_Scroll_Project/10_Scroll_Project.js
@@ -1,8 +1,9 @@
 var today = new Date();
-const nav_bar = document.querySelector("#nav");
 const date_content = document.querySelector(".year-container");
 date_content.innerText = today.getFullYear() + ".";
 
+
+const nav_bar = document.querySelector("#nav");
 document.addEventListener("scroll", function(){
     var scroll_position = document.documentElement.scrollTop;
     if(scroll_position > 80){
@@ -13,6 +14,22 @@ document.addEventListener("scroll", function(){
     }
 });
 
+const text_links = document.querySelectorAll(".link-content");
+var nav_height = nav_bar.offsetHeight;
+text_links.forEach(function(link){
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const linked_section = document.getElementById(id);
+
+        let location =  linked_section.getBoundingClientRect().y;
+        console.log(location);
+        
+        window.scrollTo({top: location, behavior: "smooth"});
+    });
+});
+
 const slide_bar = document.querySelector(".bar-container");
 const links = document.querySelector(".link-container");
 
@@ -21,4 +38,3 @@ slide_bar.addEventListener("click", show_links);
 function show_links(){
     links.classList.toggle('slide');
 };
-
